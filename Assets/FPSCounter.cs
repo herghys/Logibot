d16185fda866fe7fd4d84bb2330d12f466a45c31
Helmi Arghya Santosa <49424708+herghys@users.noreply.github.com>
2021-04-27 12:14:30 +0700

    Other Settings
    // Other Settings

---
+++ Assets/FPSCounter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FPSCounter : MonoBehaviour
+{
+    public static FPSCounter Instance = null;
+    [SerializeField] private float timer, refresh, avgFramerate;
+    [SerializeField] private bool shoFPS;
+    [SerializeField] private Text text;
+    private void Awake()
+    {
+        if (Instance != null)
+        { Destroy(gameObject); }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        Application.targetFrameRate = 60;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float timelapse = Time.smoothDeltaTime;
+        timer = timer <= 0 ? refresh : timer -= timelapse;
+
+        if (timer <= 0) avgFramerate = (int)(1f / timelapse);
+        text.text = avgFramerate.ToString();
+    }
+}
